2490febd0512c0e12dcd076ac8d702f6ccbc3fd7
Syedsarfaraz0990 <syedsarfaraz0990@gmail.com>
2025-07-11 15:22:15 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Share Host</h1>
+      <p>This is a clean Vite + React + TypeScript app.</p>
+    </div>
+  )
+}
+
+export default App
